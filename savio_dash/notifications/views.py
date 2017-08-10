bb6ac03f892b7f39cb60fcb17dd5f4dfe50abf80
Cassie Zhang <cassie.zhang@berkeley.edu>
2017-08-09 18:06:32 -0700

    Display notifications

--- savio_dash/notifications/views.py
+++ savio_dash/notifications/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib import messages
 import os, os.path, json
 
 # Create your views here.
@@ -6,4 +7,7 @@ def notifications(request):
 	title = "Notification Center"
 
 	args = { 'theTitle': title }
+
+	if 6 == 6:
+		messages.info(request, "You've used up 75 percent of your quota")
 	return render(request, 'notifications.html', args)
